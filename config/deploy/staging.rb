57690e7513ce120f41110150666f01b74d405761
thuijssoon <thijshuijssoon@Thijss-MacBook-Pro.local>
2014-03-11 13:55:28 +0100

    Set deploy_to and SSH options for staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,7 @@
 set :stage, :staging
 
+set :deploy_to, "/var/www/vhosts/z2wp-sg7m.accessdomain.com/staging.connectchristiancounseling.com"
+
 # Simple Role Syntax
 # ==================
 #role :app, %w{deploy@example.com}
@@ -14,11 +16,11 @@ server 'example.com', user: 'deploy', roles: %w{web app db}
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   # keys: %w(/home/rlisowski/.ssh/id_rsa),
+   forward_agent: true,
+   # auth_methods: %w(password)
+ }
 
 fetch(:default_env).merge!(wp_env: :staging)
 
