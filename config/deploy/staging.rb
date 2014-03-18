4b9cb0551e40224dcf4b638da33c4ee7a52a1ad9
thuijssoon <thijshuijssoon@Thijss-MacBook-Pro.local>
2014-03-18 16:17:19 +0100

    Changed domain to christiansextreatment.com

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 
-set :deploy_to, "/var/www/vhosts/z2wp-sg7m.accessdomain.com/staging.connectchristiancounseling.com"
+set :deploy_to, "/var/www/vhosts/z2wp-sg7m.accessdomain.com/staging.christiansextreatment.com"
 set :tmp_dir, "/tmp2"
 
 # Simple Role Syntax
