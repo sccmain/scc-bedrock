05a720205f6e2c3042336bb2c63cf2a2fda2a6ce
thuijssoon <thijshuijssoon@Thijss-MacBook-Pro.local>
2014-03-26 12:24:58 +0100

    Moving deploy_to definition to environment specific files

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,6 @@ set :repo_url, 'git@github.com:thuijssoon/c3.git'
 # This could be overridden in a stage config file
 set :branch, :master
 
-set :deploy_to, "/srv/www/#{fetch(:application)}/htdocs"
-
 set :log_level, :debug
 
 set :linked_files, %w{.env}
